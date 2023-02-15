ac7e5144e7f7a8fa0a39971b1c0ec331a62a8fa9
Kulal96 <59464702+Kulal96@users.noreply.github.com>
2023-02-15 21:09:14 +0530

    Create scoreboard.py

---
+++ ping_pong_game/scoreboard.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.l_score=0
+        self.r_score=0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.update_scoreboard()
+        
+    
+    def update_scoreboard(self):
+        self.goto(100,200)
+        self.write(self.l_score,align="left", font=("Arial", 75, "normal"))
+        self.goto(-100,200)
+        self.write(self.r_score,align="left", font=("Arial", 75, "normal"))
+
+    
+    def l_point(self):
+        self.l_score+=1
+        self.clear()
+        self.update_scoreboard()
+    
+    def r_point(self):
+        self.r_score+=1
+        self.clear()
+        self.update_scoreboard()
+
+
