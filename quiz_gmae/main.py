985605b34c7d334971270aeea7c2e9085bdd90f9
Kulal96 <59464702+Kulal96@users.noreply.github.com>
2023-02-16 23:11:33 +0530

    Create main.py

---
+++ quiz_gmae/main.py
@@ -0,0 +1,17 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank=[]
+
+for item in question_data:
+    question_text=item["text"]
+    question_answer=item["answer"]
+    new_question=Question(question_text,question_answer)
+    question_bank.append(new_question)
+
+quiz=QuizBrain(question_bank)
+FLAG=True
+
+while quiz.still_has_question():
+    quiz.next_question()
